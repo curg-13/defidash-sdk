7766f7f2194cdda6a9afadcf52567d908291f509
Jeongseup <seup87@gmail.com>
2026-01-30 22:58:10 +0900

    Remove unused scallop libs

--- src/lib/scallop/index.ts
+++ src/lib/scallop/index.ts
@@ -1,20 +1,7 @@
-// Re-export only unique items to avoid conflicts
-export {
-  SCALLOP_ADDRESSES,
-  SCALLOP_COIN_TYPES,
-  SCALLOP_COIN_DECIMALS,
-  getCoinType,
-  getCoinDecimals,
-} from "./scallop-addresses";
-export {
-  borrowFlashLoan,
-  repayFlashLoan,
-  calculateFlashLoanFee,
-} from "./flash-loan";
+// Scallop Flash Loan Client
 export { ScallopFlashLoanClient } from "./flash-loan-client";
-export { ScallopBuilder, ScallopTxBlock } from "./scallop-builder";
 export type {
   ScallopCoreIds,
   ScallopCoinTypes,
-  ScallopBuilderOptions,
-} from "./scallop-builder";
+  ScallopFlashLoanClientOptions,
+} from "./flash-loan-client";
